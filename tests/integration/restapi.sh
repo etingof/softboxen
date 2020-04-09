300d57b50be85c9aa68bcca731eec27716f7b61f
Ilya Etingof <etingof@gmail.com>
2020-04-09 07:33:51 +0200

    Improve REST API invocation from tox

--- tests/integration/restapi.sh
+++ tests/integration/restapi.sh
@@ -10,11 +10,47 @@ set -e
 
 RESTAPI_CONF=$(mktemp /tmp/softboxen.XXXXXX)
 
+USAGE=$(cat << EOF
+Usage: $0 [options]
+  --help                          Usage help message
+  --recreate-db                   Populate REST API DB with some data
+  --keep-running                  Keep REST API servers running
+EOF)
+
+
+recreate_db=no
+keep_running=no
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+
+    case $key in
+        --help)
+            echo Synopsis: invoke REST API servers, optionally initialize
+            echo the underlying DB.
+            echo "$USAGE"
+            exit 0
+            ;;
+        --recreate-db)
+            recreate_db=yes
+            shift # past argument
+            ;;
+        --keep-running)
+            keep_running=yes
+            shift # past argument
+            ;;
+    esac
+done
+
 sed -e 's/DEBUG = True/DEBUG = False/g' $(pwd)/conf/softboxen.conf > $RESTAPI_CONF
 
-softboxen-restapi \
-    --config $RESTAPI_CONF \
-    --recreate-db
+if [ $recreate_db = "yes" ]; then
+    softboxen-restapi \
+        --config $RESTAPI_CONF \
+        --recreate-db
+fi
 
 softboxen-restapi \
     --config $RESTAPI_CONF &
@@ -33,6 +69,12 @@ echo Waiting for dust to settle down...
 
 sleep 10
 
-bash conf/bootstraps/create-box-port-vlan.sh
+if [ $recreate_db = "yes" ]; then
+    bash conf/bootstraps/create-box-port-vlan.sh
+fi
+
+if [ $keep_running = "yes" ]; then
+    cat -
+fi
 
 exit 0
