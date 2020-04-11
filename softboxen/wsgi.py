30f9e9fcbb499b42b56f3dae32ea68fa5686516b
Ilya Etingof <etingof@gmail.com>
2020-04-11 12:00:02 +0200

    Add WSGI hook

---
+++ softboxen/wsgi.py
@@ -0,0 +1,10 @@
+#
+# This file is part of softboxen software.
+#
+# Copyright (c) 2020, Ilya Etingof <etingof@gmail.com>
+# License: https://github.com/etingof/softboxen/LICENSE.rst
+#
+# REST API server WSGI app
+#
+from softboxen.api import app  # noqa
+from softboxen.api import views  # noqa
